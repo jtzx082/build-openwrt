613c7f57df4a08b5f6d4b283695677afddaa30a1
jtzx082 <yujq804@gmail.com>
2025-05-31 21:05:02 +0800

    Update diy-part2.sh

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -12,11 +12,11 @@
 # sed -i 's/luci-theme-bootstrap/luci-theme-material/g' ./feeds/luci/collections/luci/Makefile
 
 # Add autocore support for armvirt
-sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
+# sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
 
 # Set etc/openwrt_release
-sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/lean/default-settings/files/zzz-default-settings
-echo "DISTRIB_SOURCECODE='lede'" >>package/base-files/files/etc/openwrt_release
+#sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/lean/default-settings/files/zzz-default-settings
+#echo "DISTRIB_SOURCECODE='lede'" >>package/base-files/files/etc/openwrt_release
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
 # sed -i 's/192.168.1.1/192.168.31.4/g' package/base-files/files/bin/config_generate
@@ -29,7 +29,86 @@ echo "DISTRIB_SOURCECODE='lede'" >>package/base-files/files/etc/openwrt_release
 # ------------------------------- Other started -------------------------------
 #
 # Add luci-app-amlogic
-svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+#svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+
+# 移除要替换的包
+#rm -rf feeds/packages/net/v2ray-geodata
+
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+
+# 添加额外插件
+#git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
+# 添加主题
+#git clone -b js --single-branch https://github.com/gngpp/luci-theme-design package/luci-theme-design
+# git clone https://github.com/derisamedia/luci-theme-alpha.git package/luci-theme-alpha
+
+# 添加测速插件
+git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
+# 添加 万能推送
+# git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
+# 添加关机插件
+#git clone https://github.com/VPN-V2Ray/luci-app-poweroff.git package/luci-app-poweroff
+# 添加passwall2
+git clone https://github.com/xiaorouji/openwrt-passwall2.git package/luci-app-passwall2
+# 添加应用过滤
+#git clone  https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+#加入turboacc
+#curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh
+#chmod -R 777 add_turboacc.sh
+#bash add_turboacc.sh --no-sfe
+# 添加分区扩容插件
+git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
+
+
+echo "
+# 主题
+#CONFIG_PACKAGE_luci-theme-design=y
+
+CONFIG_PACKAGE_luci-theme-argon=y
+
+#CONFIG_PACKAGE_luci-theme-material=y
+
+#CONFIG_PACKAGE_luci-theme-openwrt-2020=y
+
+#CONFIG_PACKAGE_luci-theme-alpha=y
+
+# 测速插件
+CONFIG_PACKAGE_luci-app-netspeedtest=y
+
+# 万能推送
+#CONFIG_PACKAGE_luci-app-pushbot=y
+
+# TurboAcc
+#CONFIG_PACKAGE_luci-app-turboacc=y
+
+# 应用过滤
+#CONFIG_PACKAGE_luci-app-oaf=y
+
+# 添加passwall2
+CONFIG_PACKAGE_luci-app-passwall2=y
+
+# 添加分区扩容插件
+CONFIG_PACKAGE_luci-app-partexp=y
+
+" >> .config
+
+# 修改默认IP
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+
+# 修改默认子网掩码
+sed -i 's/255.255.255.0/255.255.252.0/g' package/base-files/files/bin/config_generate
+
+# 修改默认主题
+#sed -i 's/luci-theme-openwrt-2020/luci-theme-alpha/g' feeds/luci/collections/luci/Makefile
 
 # Fix runc version error
 # rm -rf ./feeds/packages/utils/runc/Makefile
