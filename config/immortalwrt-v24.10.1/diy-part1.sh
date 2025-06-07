2766c97de67136650066f5057b29e8f98fc6a54a
jtzx082 <yujq804@gmail.com>
2025-06-07 21:41:00 +0800

    Update diy-part1.sh

--- config/immortalwrt-v24.10.1/diy-part1.sh
+++ config/immortalwrt-v24.10.1/diy-part1.sh
@@ -10,7 +10,7 @@
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 echo 'src-git netspeedtest https://github.com/sirpdboy/luci-app-netspeedtest' >>feeds.conf.default
 echo 'src-git partexp https://github.com/sirpdboy/luci-app-partexp.git' >>feeds.conf.default
-echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
+#echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
 
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
